2b4ce339fdbe0002f6761bc0a4ea1af8366b9a88
Hilton Junior <146493108+Devmmx166@users.noreply.github.com>
2023-10-15 01:23:29 -0300

    Modulo primeiros passos v 0.1

---
+++ variaveis.js
@@ -0,0 +1,13 @@
+var nome = "Hilton"
+let idade = 36
+const altura = 1.67
+
+
+console.log(`Nome:`+nome , `Altura: `+altura,`idade: `+ idade)
+
+//var bariavel que pode ser reatribuida pouco utilizada
+
+//let geralmente é utilizada no lugar da var
+
+// utilizado para valores constantes mais utilizado
+
